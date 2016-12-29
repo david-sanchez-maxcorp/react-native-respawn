a50f140288a510b82a8070707b51b4c8f616cc96
agarcia_21 <agarcia_21@att.net>
2016-12-28 16:56:41 -0800

    using spread operator instead of Object.assign

--- app/containers/CounterList/styles.js
+++ app/containers/CounterList/styles.js
@@ -1,40 +1,35 @@
 import { StyleSheet } from 'react-native';
 import flexbox from '../../_shared/styles/flexbox';
-import colors from '../../_shared/styles/colors';
 
-const simpleButton = Object.assign({},
-  flexbox.flex,
-  flexbox.layoutAlignCenterCenter, {
-    backgroundColor: '#FFFFFF',
-    height: 40,
-    margin: 12
-  }
-);
+const simpleButton = {
+  ...flexbox.flex,
+  ...flexbox.layoutAlignCenterCenter,
+  backgroundColor: '#FFFFFF',
+  height: 40,
+  margin: 12
+};
 
 const styles = StyleSheet.create({
-  counterContainer: Object.assign({},
-    flexbox.container,
-    flexbox.layoutColumn,
-    flexbox.layoutAlignStartStretch, {
-      /* any extra custom properties */
-    }
-  ),
+  counterContainer: {
+    ...flexbox.container,
+    ...flexbox.layoutColumn,
+    ...flexbox.layoutAlignStartStretch
+    /* any extra custom properties */
+  },
 
-  buttonGroup: Object.assign({},
-    flexbox.layoutRow,
-    flexbox.layoutAlignCenterCenter, {
-      /* any extra custom properties */
-    }
-  ),
+  buttonGroup: {
+    ...flexbox.layoutRow,
+    ...flexbox.layoutAlignCenterCenter
+    /* any extra custom properties */
+  },
 
   simpleButton,
 
-  button: Object.assign({},
-    simpleButton, {
-      borderWidth: 1,
-      borderColor: '#0D47A1'
-    }
-  ),
+  button: {
+    ...simpleButton,
+    borderWidth: 1,
+    borderColor: '#0D47A1'
+  },
 
   buttonText: {
     color: '#0D47A1',
